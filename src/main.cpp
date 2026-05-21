19ca81fcc5b837e49f0ba8b5c1afd3839d31c2f1
Notinon <nathanvansteenberge158@gmail.com>
2026-05-21 14:41:41 +0200

    YEYEYEYEYEYEYEYEY EI GOT THIS SHIT FUCKING WORKING WOOOOOHOOOOO!!!!!

--- src/main.cpp
+++ src/main.cpp
@@ -3,11 +3,12 @@
 #include <gumbo.h>
 
 struct Memory_Structs{ 
-    char* memory; // The data we write
-    size_t size; // how much of the data we write
+    char* memory = nullptr; // The data we write
+    size_t size = 0; // how much of the data we write
 };
 
 void search_for_links(GumboNode* node);
+void search_for_script_links(GumboNode *node);
 
 Memory_Structs memory_struct;
 
@@ -19,12 +20,17 @@ int main(){
     CURL* curl = curl_easy_init(); // init curl easy handle
     curl_easy_setopt(curl, CURLOPT_URL, "https://zeppuzzle.lennlepez.workers.dev/"); // test site
     curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, write_data); // The function with which we store the data
-    curl_easy_setopt(curl, CURLOPT_WRITEDATA, memory_struct); 
+    curl_easy_setopt(curl, CURLOPT_WRITEDATA, &memory_struct); 
     CURLcode result = curl_easy_perform(curl);
+    if(result == CURLE_OK && memory_struct.memory != nullptr){
+        GumboOutput* output = gumbo_parse(memory_struct.memory);
+        search_for_links(output->root);
+        search_for_script_links(output->root);
+        gumbo_destroy_output(&kGumboDefaultOptions, output);
+    }else{
+        std::cerr << "CURL ERROR: " << curl_easy_strerror(result);
+    }
     
-    GumboOutput* output = gumbo_parse(memory_struct.memory);
-    search_for_links(output->root);
-    gumbo_destroy_output(&kGumboDefaultOptions, output);
 
 
     curl_global_cleanup();
@@ -46,6 +52,22 @@ void search_for_links(GumboNode *node){
     }
 }
 
+void search_for_script_links(GumboNode *node){
+    if(node->type != GUMBO_NODE_ELEMENT){ // checks to see if the thing that is currently in the node is even a fucking node
+        return; // NOTANODEGETTHEFUKCKOTUO
+    }
+    GumboAttribute* scripts; 
+
+    if(node->v.element.tag == GUMBO_TAG_SCRIPT && (scripts = gumbo_get_attribute(&node->v.element.attributes, "src"))){ // checks for if the things at that node is a script
+        std::cout << scripts->value << '\n'; // YAAAYAYAYAYAYAYAYA IS A FUCJ9IBG 9P SCRIPT!!!!!!
+    }
+
+    GumboVector* children = &node->v.element.children; // Walk farther into the HTML5 tree
+    for(unsigned int i = 0; i < children->length; i++){
+        search_for_script_links(static_cast<GumboNode*>(children->data[i]));
+    }
+}
+
 size_t write_data(char *buffer, size_t size, size_t nmemb, void *userp)
 {
 
@@ -55,7 +77,8 @@ size_t write_data(char *buffer, size_t size, size_t nmemb, void *userp)
     char *ptr = (char*)realloc(mem->memory, mem->size + realsize + 1); // Allocates memory for memory with hopefully enough size
     if(!ptr){
         // Not enough/ran out of memory
-        std::cout << "Not enough memory (Realloc returned NULL)";
+        std::cerr << "Not enough memory (Realloc returned NULL)";
+        return 0;
     }
 
     mem->memory = ptr;
