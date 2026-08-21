06d859fffd04828c47e38eee12acf004026b7e20
Notinon <nathanvansteenberge158@gmail.com>
2026-08-21 09:56:12 +0200

    figuring out robot.txt parsing

--- src/main.cpp
+++ src/main.cpp
@@ -4,40 +4,51 @@
 #include <unordered_set>
 #include <queue>
 #include <string>
+#include <thread> 
+#include <chrono>
+#include "robots.h"
+#include <vector>
 
-struct Memory_Structs{ 
+struct Memory_Structs{
     char* memory = nullptr; // The data we write
     size_t size = 0; // how much of the data we write
 };
 
-void search_for_links(GumboNode* node);
+bool is_Crawlable(const std::string contents, const std::string user_agent, const std::string url_path);
+
+void search_for_links(GumboNode* node, int parent_depth);
 void search_for_script_links(GumboNode* node);
 void search_for_images(GumboNode* node);
 
-struct Crawl_task{
-    std::string url;
-    int depth;
+struct Crawl_task{ // Crawlers variables
+    std::string url; // Crawled url
+    int depth; 
 };
 
-void Crawl_process(CURL* curl);
+const std::string user_agent =  "WebCrawler/1.0 (https://github.com/Notinon/WebCrawler_cURL); Nathanvansteenberge158@gmail.com";
+
+void Crawl_process(CURL* curl, std::queue<Crawl_task> &urls);
 std::queue<Crawl_task> urls;
 std::unordered_set<std::string> visited_urls;
+std::unordered_set<std::string> robotstxts;
 int current_depth = 0;
 
 Memory_Structs memory_struct;
 
 size_t write_data(char *buffer, size_t size, size_t nmemb, void *userp);
+void reset_memory(Memory_Structs *mem);
 
-const int MAX_DEPTH;
+const int MAX_DEPTH = 4;
 
 int main(){
     curl_global_init(CURL_GLOBAL_ALL); // initialise curl with all flags enabled
     
     CURL* curl = curl_easy_init(); // init curl easy handle
-    urls.push({"https://zeppuzzle.lennlepez.workers.dev/"}); // test site first site
+    urls.push({"https://zeppuzzle.lennlepez.workers.dev/", current_depth}); // test site first site
+    curl_easy_setopt(curl, CURLOPT_USERAGENT, user_agent);
     curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, write_data); // The function with which we store the data
     curl_easy_setopt(curl, CURLOPT_WRITEDATA, &memory_struct); 
-    Crawl_process(curl);
+    Crawl_process(curl, urls);
     
     free(memory_struct.memory);
     curl_easy_cleanup(curl);
@@ -45,19 +56,31 @@ int main(){
     return 0;
 }
 
-void search_for_links(GumboNode *node){
+bool is_Crawlable(const std::string& contents, const std::string& user_agent, const std::string& url_path){
+    googlebot::RobotsMatcher matcher;
+    
+    std::vector<std::string> user_agents;
+    user_agents.push_back(user_agent);
+
+    return matcher.AllowedByRobots(contents, &user_agents, url_path);
+}
+
+void search_for_links(GumboNode *node, int parent_depth)
+{
     if(node->type != GUMBO_NODE_ELEMENT){
         return; 
     }
     GumboAttribute* href;
     if(node->v.element.tag == GUMBO_TAG_A && (href = gumbo_get_attribute(&node->v.element.attributes, "href"))){
-        current_depth += 1;
-        urls.push({href->value});
+        int next_depth = parent_depth+1;
+        if(next_depth <= MAX_DEPTH){
+            urls.push({href->value, next_depth});
+        }
     }
 
     GumboVector* children = &node->v.element.children;
     for(unsigned int i = 0; i < children->length; i++){
-        search_for_links(static_cast<GumboNode*>(children->data[i]));
+        search_for_links(static_cast<GumboNode*>(children->data[i]), parent_depth);
     }
 }
 
@@ -116,22 +139,54 @@ size_t write_data(char *buffer, size_t size, size_t nmemb, void *userp)
     return realsize;
 }
 
-void Crawl_process(CURL* curl){
-    curl_easy_setopt(curl ,CURLOPT_URL, urls.front().url.c_str());
-    CURLcode result = curl_easy_perform(curl);
-    if(result == CURLE_OK && memory_struct.memory != nullptr){
-        GumboOutput* output = gumbo_parse(memory_struct.memory);
-        search_for_links(output->root);
-        search_for_script_links(output->root);
-        gumbo_destroy_output(&kGumboDefaultOptions, output);
-    }else{
-        std::cerr << "CURL ERROR: " << curl_easy_strerror(result);
+void reset_memory(Memory_Structs *mem){
+    if (mem->memory){
+        free(mem->memory);
+        mem->memory = nullptr;
     }
-    if (urls.empty() == false){
-        std::cout << urls.front().depth << '\t' << urls.front().url << '\n';
+    mem->size = 0;
+}
+
+void Crawl_process(CURL* curl, std::queue<Crawl_task> &urls){
+    using namespace std::chrono_literals;
+    using namespace std::this_thread;
+    using std::chrono::system_clock;
+
+    while(!urls.empty()){
+        Crawl_task current = urls.front();
         urls.pop();
-    }else{
-        return;
+
+        if(visited_urls.count(current.url)){
+            continue;
+        }
+        visited_urls.insert(current.url);
+
+        current.url = current.url+"/robots.txt";
+        curl_easy_setopt(curl, CURLOPT_URL, current.url);
+        CURLcode result_robottxt = curl_easy_perform(curl);
+        if (result_robottxt == CURLE_OK && memory_struct.memory != nullptr){
+            robotstxts.insert(memory_struct.memory);
+        }
+         
+        std::cout << "Next crawling: " << current.url << ' ' << "Depth: " << current.depth << '\n';
+        std::this_thread::sleep_until(system_clock::now() + 2s);
+
+        reset_memory(&memory_struct);
+        curl_easy_setopt(curl, CURLOPT_URL, current.url.c_str());
+        CURLcode result = curl_easy_perform(curl);
+
+        if(result == CURLE_OK && memory_struct.memory != nullptr){
+            GumboOutput *output = gumbo_parse(memory_struct.memory);
+
+            search_for_links(output->root, current.depth);
+            search_for_script_links(output->root);
+            search_for_images(output->root);
+
+            gumbo_destroy_output(&kGumboDefaultOptions, output);
+        }else{
+            std::cerr << "CURL ERROR: " << current.url << ": " << curl_easy_strerror(result) << '\n';
+        }
+        
+
     }
-    
 }
